12c773c76555732c54be123eed173fdeda12abe4
celianiet <celianiet@gmail.com>
2019-04-08 18:25:25 +0100

    commit authors

--- src/cctair/setup.java
+++ src/cctair/setup.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package cctair;
 
 
@@ -12,7 +8,7 @@ import java.util.Scanner;
 
 /**
  *
- * @author Italo Marcius
+ * @author Italo, Celia and Aman.
  */
 public class setup {
     
